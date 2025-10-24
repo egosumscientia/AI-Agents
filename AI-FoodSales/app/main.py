07f9ee014f9a3890ee03dd98ff93359a5c2cc75a
pauloenrique <egosumscientia@gmail.com>
2025-10-24 09:22:26 -0500

    fs running

--- AI-FoodSales/app/main.py
+++ AI-FoodSales/app/main.py
@@ -5,6 +5,7 @@ app = FastAPI(title="Food Sales Agent API", version="1.0.0")
 app.include_router(chat.router)
 app.include_router(health.router)
 
+
 @app.get("/")
 async def root():
     return {"message": "API del Asistente de Ventas en línea ✅"}
